a1eea75556de826470ce06c277b79eebc808b5d7
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-04-04 16:50:47 +0200

    Fixed pom and ApplicationInitializer

---
+++ src/main/java/com/example/config/ApplicationInitializer.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext arg0) throws ServletException {
+		
+		
+	}
+
+}
