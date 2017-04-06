fb6a0991ac7ae8bb12789b0190639b23686c3aa4
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-04-06 12:13:32 +0200

    Fixed Book & User model

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,9 +1,52 @@
 package com.example.model;
 
-class Book {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "books")
+class Book extends BaseEntity{
 	
+//	@Column(name="title")
+	@NotNull
+	@Size(min=3, max=255, message = "Podaj dlugosc od {min} do {max}")
 	private String title;
+	
+	@NotNull
+	@Size(min=3, max=255)
 	private String author;
 	
-	private Integer available;
+	@Min(0)
+	private Integer available; //liczba dostepnych ksiazek
+	
+	public Book() {
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(Integer available) {
+		this.available = available;
+	}
 }
--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,14 +1,79 @@
 package com.example.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+@Table(name="users")
 public class User {
 
+	public enum Role {
+		USER, ADMIN
+	}
+	
+	@Column(name = "first_name", nullable = false, length = 455)
 	private String firstName;
 	
+	@Column(name = "last_name", nullable = false)
 	private String lastName;
 	
+	@Column( nullable = false, unique = true)
+	@Email
 	private String email;
 	
+	@Column( nullable = false)
 	private String password;
 	
-	//+rola
+	@Enumerated(EnumType.STRING)
+	@Column( nullable = false)
+	private Role role;
+	
+	public User() {
+		
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
