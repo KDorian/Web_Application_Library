0a9d84890bab94a9b294e50480d106b4793aa002
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-04-06 11:22:45 +0200

    Add Bean EntityManagerFactory at HibernateConfig

--- src/main/java/com/example/config/HibernateConfig.java
+++ src/main/java/com/example/config/HibernateConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import java.util.Properties;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -11,11 +13,13 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @PropertySource(value="classpath:hibernate.properties")
-@EnableJpaRepositories(basePackages = "com.ak.dao")
+@EnableJpaRepositories(basePackages = "com.example.dao")
 public class HibernateConfig {
 	
 	@Autowired
@@ -35,9 +39,25 @@ public class HibernateConfig {
 	//metoda z konfiguracja hinernatea w ktorej rowniez przakazujemy informacje w ktorym pakiecie znajduja sie encje
 	@Bean
 	public EntityManagerFactory entityManagerFactory() {
-		//HibernateJpaVendorAdapter vendorAdapter = new 
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		
-		return null;
+		Properties properties =  new Properties();
+		properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+		properties.put("hibernate.generate_statistic", environment.getProperty("hibernate.generate_statistic"));
+		
+		//glowny obiekt fabryki beanow
+		LocalContainerEntityManagerFactoryBean factorybean = new LocalContainerEntityManagerFactoryBean();
+		factorybean.setPackagesToScan("com.example.model");
+		factorybean.setJpaVendorAdapter(vendorAdapter);
+		factorybean.setJpaProperties(properties);
+		factorybean.setDataSource(dataSource());
+		
+		//produkcja beanow encyjnych byla mozliwa dopiero gdy zostanie wykonana cala konfiguracja
+		factorybean.afterPropertiesSet();
+		
+		return factorybean.getObject();
 	}
 	
 	@Bean
@@ -47,3 +67,4 @@ public class HibernateConfig {
 		
 		return manager;
 	}
+}
