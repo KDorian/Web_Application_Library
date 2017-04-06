9eb23eb22d6689d70a1cb72156e474f82c7c3fb9
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-04-07 00:00:19 +0200

    Add UserController, fix User & EmailServiceImpl

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,79 +1,83 @@
 package com.example.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
-
 import org.hibernate.validator.constraints.Email;
 
+
+import javax.persistence.*;
+
 @Entity
-@Table(name="users")
-public class User extends BaseEntity{
+@Table(name = "users")
+public class User extends BaseEntity {
 
-	public enum Role {
-		USER, ADMIN
-	}
-	
-	@Column(name = "first_name", nullable = false, length = 455)
-	private String firstName;
-	
-	@Column(name = "last_name", nullable = false)
-	private String lastName;
-	
-	@Column( nullable = false, unique = true)
-	@Email
-	private String email;
-	
-	@Column( nullable = false)
-	private String password;
-	
-	@Enumerated(EnumType.STRING)
-	@Column( nullable = false)
-	private Role role;
-	
-	public User() {
-		
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public enum Role {
+        USER, ADMIN
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    @Column(name = "first_name", nullable = false, length = 455)
+    private String firstName;
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
 
-	public String getEmail() {
-		return email;
-	}
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    @Column(nullable = false)
+    private String password;
 
-	public String getPassword() {
-		return password;
-	}
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.USER;
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public User() {
+    }
 
-	public Role getRole() {
-		return role;
-	}
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 
-	public void setRole(Role role) {
-		this.role = role;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
